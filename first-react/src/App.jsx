89f9139d3bc8c4bb0ebc1840071e444192abfe73
Malik Qadeer <qadeerali2320@gmail.com>
2026-06-25 16:23:55 +0500

    resolve bugs

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Navbar from './components/Navbar';
-import Textform from './components/Textform';
+import Textform from './components/TextForm';
 function App() {
   const [count, setCount] = useState(0)
 
